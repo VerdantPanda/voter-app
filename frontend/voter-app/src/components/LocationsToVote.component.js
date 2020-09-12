17cd4c0f2658f4b91a4f89aff66cfa8e9aa2ebec
Mohammed Abaker <mohamme@nichols-ve703-03319.apn.wlan.private.upenn.edu>
2020-09-12 18:30:00 -0400

    added location link

--- frontend/voter-app/src/components/LocationsToVote.component.js
+++ frontend/voter-app/src/components/LocationsToVote.component.js
@@ -1,24 +1,50 @@
-import React  from "react";
+import React ,{useState} from "react";
+import { Form } from "react-bootstrap/";
+import states_names from "../static_data/states_names";
+import Button from "@material-ui/core/Button";
 import  GoogleApiWrapper  from "./GoogleApiWrapper" ;
-import Iframe from 'react-iframe'
+
 
 //AIzaSyAEq-OyHRzCJ1pvQKV9Qwq0INafDci8G3A
 
 const LocationsToVote  = () => {
+  const list = states_names.map((state) => <option key={state}>{state}</option>);
+  const [state, changeState] = useState (""); 
+  const getState = () => {
+    try {
+    const {data} = await axios.get("https://voterapppennapps.herokuapp.com/api/state/" + state) ;
+    window.location.href = data.locationLink;
+    changeState
+      } catch (error) {
+        console.log(error);
+      }
+  }
 
-
+  const handleChange = (event) => {
+    changeState(event.target.value);
+  }
   return ( 
     <div>
-        <p>You are on the LocationsToVote component!</p>  
-       
-<Iframe url="https://voterlookup.elections.ny.gov/"
-        width="450px"
-        height="450px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"/>
-       
+        <p>You are on the LocationsToVote component!</p>
+        <Form>
+          <Form.Group controlId="stateSelector">
+            <Form.Label>Example select</Form.Label>
+            <Form.Control as="select" size="lg" onChange={handleChange}>
+              {list}
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <Button
+        variant="contained"
+        onClick={() => {
+          getState
+        }}
+        target="_blank"
+        color="primary"
+      >
+        Register to Vote in {this.state.currState ?? "your state"}!
+      </Button>
+
       </div>
    );
 }
