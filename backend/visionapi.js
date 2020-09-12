8cd0bb807f6df9fd1b9b6e83e5e9030989f4b0b1
neehalsquared <neehal@sas.upenn.edu>
2020-09-12 00:35:25 -0400

    visionapiupdate

--- backend/visionapi.js
+++ backend/visionapi.js
@@ -1,16 +1,26 @@
 function VisionAPI(image) {
   const vision = require('@google-cloud/vision');
+  const states_names = require('../frontend/voter-app/src/static_data/states_names.js');
   const client = new vision.ImageAnnotatorClient({
       keyFilename: 'APiKey.json'
   });
   client
     .documentTextDetection(image)
     .then(response => {
-      return(response);
+      for (state of states_names) {
+        if (response[0].fullTextAnnotation.text.includes(state)) {
+          return state;
+        }
+      }
+      return('No state found!');
     })
     .catch(err => {
       return('Image error!!!');
     });
 
 }
-export default VisionAPI;
+
+module.exports = VisionAPI;
+
+
+
--- frontend/voter-app/src/components/ScanBallot.component.js
+++ frontend/voter-app/src/components/ScanBallot.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import VisionAPI from "../../../../backend/visionapi.js";
 // need to create function that actually scans the ballot, returning a picture
 // so that it can return the necessary information
+
 export default class ScanBallot extends Component {
   render() {
     return (
