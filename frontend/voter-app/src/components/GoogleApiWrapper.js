af561652c7bbf0f83eb2daa20078439e175d8c36
Ibrahim Fagbamila <ibrahim.fagbamila@aol.com>
2020-09-12 16:51:43 -0400

    small edits

--- frontend/voter-app/src/components/GoogleApiWrapper.js
+++ frontend/voter-app/src/components/GoogleApiWrapper.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 const mapStyles = {
-  width: '33vw',
-  height: "33vh"
+  width: "33vw",
+  height: "33vh",
 };
 
 export class MapContainer extends Component {
@@ -13,17 +13,15 @@ export class MapContainer extends Component {
         google={this.props.google}
         zoom={14}
         style={mapStyles}
-        initialCenter={
-          {
-            lat: 39.952208,
-            lng: -75.192256
-          }
-        }
+        initialCenter={{
+          lat: 39.952208,
+          lng: -75.192256,
+        }}
       />
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyAEq-OyHRzCJ1pvQKV9Qwq0INafDci8G3A"
+  apiKey: "AIzaSyAEq-OyHRzCJ1pvQKV9Qwq0INafDci8G3A",
 })(MapContainer);
