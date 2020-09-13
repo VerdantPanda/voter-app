c4c8209251e4f88fe5aaf4af1c5284149c97b567
Ibrahim Fagbamila <ibrahim.fagbamila@aol.com>
2020-09-13 05:04:11 -0400

    Update VoterSuppression.component.js

--- frontend/voter-app/src/components/VoterSuppression.component.js
+++ frontend/voter-app/src/components/VoterSuppression.component.js
@@ -23,6 +23,7 @@ export default class VoterSuppression extends Component {
     ));
     this.handleChange = this.handleChange.bind(this);
     this.handleChangeState = this.handleChangeState.bind(this);
+    this.onClick = this.onClick.bind(this);
     this.state = {
       state: "Alabama",
       newsTitle: [""],
@@ -34,6 +35,38 @@ export default class VoterSuppression extends Component {
     };
   }
 
+  onClick(event) {
+    console.log("here");
+    var url1 =
+      "https://news.google.com/rss/search?q=Voter%20Suppression%20Georgia&hl=en-PK&gl=PK&ceid=PK:en";
+    axios
+      .get("http://localhost:3001/api/state/news?state=" + this.state.state)
+      .then((resp) => {
+        console.log(resp);
+        var curr = resp.data.toString();
+        var arrTitle = [""];
+        var arrLink = [""];
+        while (curr.indexOf("<link>") != -1) {
+          var first = curr.indexOf("<title>");
+          var last = curr.indexOf("</title>");
+
+          arrTitle.push(curr.substring(first + 7, last));
+
+          var first = curr.indexOf("<link>");
+          var last = curr.indexOf("</link>");
+
+          arrLink.push(curr.substring(first + 6, last));
+
+          curr = curr.substring(last + 7);
+        }
+        this.setState({ newsLink: arrLink, newsTitle: arrTitle });
+        console.log(arrLink);
+
+        this.setState({ result: curr });
+      });
+
+    return null;
+  }
   handleChange(event) {
     console.log("change occuredn in form");
     console.log(event.target.value);
@@ -56,21 +89,11 @@ export default class VoterSuppression extends Component {
           Days left.
         </h1>
         <p>Get news about voter suppression in your state below.</p>
-        <Iframe
-          url="https://data.census.gov/cedsci/profile?g=0400000US42"
-          width="100%"
-          height="450px"
-          id="food"
-          className="yes"
-          display="initial"
-          position="relative"
-          sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts"
-        />
 
         {/*TODO: Consider replacing whith AutoComplete from Material UI instead(has already been imported via npm install.)  */}
         <Form>
           <Form.Group controlId="stateSelector">
-            <Form.Label>Example select</Form.Label>
+            <Form.Label>Select State</Form.Label>
             <Form.Control
               as="select"
               size="lg"
@@ -82,41 +105,7 @@ export default class VoterSuppression extends Component {
 
           <Button
             variant="contained"
-            onClick={() => {
-              console.log("here");
-              var url1 =
-                "https://news.google.com/rss/search?q=Voter%20Suppression%20Georgia&hl=en-PK&gl=PK&ceid=PK:en";
-              axios
-                .get(
-                  "http://localhost:3001/api/state/news?state=" +
-                    this.state.state
-                )
-                .then((resp) => {
-                  console.log(resp);
-                  var curr = resp.data.toString();
-                  var arrTitle = [""];
-                  var arrLink = [""];
-                  while (curr.indexOf("<link>") != -1) {
-                    var first = curr.indexOf("<title>");
-                    var last = curr.indexOf("</title>");
-
-                    arrTitle.push(curr.substring(first + 7, last));
-
-                    var first = curr.indexOf("<link>");
-                    var last = curr.indexOf("</link>");
-
-                    arrLink.push(curr.substring(first + 6, last));
-
-                    curr = curr.substring(last + 7);
-                  }
-                  this.setState({ newsLink: arrLink, newsTitle: arrTitle });
-                  console.log(arrLink);
-
-                  this.setState({ result: curr });
-                });
-
-              return null;
-            }}
+            onClick={this.onClick}
             target="_blank"
             color="primary"
           >
@@ -139,6 +128,18 @@ export default class VoterSuppression extends Component {
             );
           })}
         </ListGroup>
+        <br></br>
+        <br></br>
+        <Iframe
+          url="https://data.census.gov/cedsci/profile?g=0400000US42"
+          width="100%"
+          height="450px"
+          id="food"
+          className="yes"
+          display="initial"
+          position="relative"
+          sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts"
+        />
         <br></br>
         <br></br>
       </div>
