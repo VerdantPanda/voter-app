e52dea1f8344c44934900178f9825486998ec406
Ibrahim Fagbamila <ibrahim.fagbamila@aol.com>
2020-09-12 20:24:49 -0400

    Front End pretty now.

--- frontend/voter-app/src/App.js
+++ frontend/voter-app/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./components/routerStyles.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 import { Redirect } from "react-router-dom";
+import {
+  Container as ContainerBootStrap,
+  Navbar as NavbarBootStrap,
+  Nav,
+} from "react-bootstrap";
 
 import Navbar from "./components/navbar.component";
 import ScanBallot from "./components/ScanBallot.component";
@@ -15,31 +27,93 @@ import LocationsToVote from "./components/LocationsToVote.component";
 import Home from "./components/Home.component";
 import Container from "@material-ui/core/Container";
 import Gradient from "./components/Gradient";
+import { Fade, Slide, Zoom } from "react-reveal";
+
+const routes = [
+  //{ path: "/", name: "", Component: "" },
+  { path: "/Home", name: "", Component: Home },
+  { path: "/ScanBallot", name: "Scan Ballot", Component: ScanBallot },
+  {
+    path: "/VoterSuppression",
+    name: " Voter Suppression",
+    Component: VoterSuppression,
+  },
+  { path: "/Volunteer", name: "Volunteer", Component: Volunteer },
+  {
+    path: "/RegisterToVote",
+    name: "Register To Vote",
+    Component: RegisterToVote,
+  },
+  { path: "/TrackBallot", name: "Track Ballot", Component: TrackBallot },
+  { path: "/News", name: "News", Component: News },
+  {
+    path: "/AddressVerification",
+    name: "Verify Address",
+    Component: AddressVerification,
+  },
+  {
+    path: "/LocationsToVote",
+    name: "Locations To Vote",
+    Component: LocationsToVote,
+  },
+];
 
 function App() {
   return (
     <div className="test">
+      {" "}
       <Router>
-        {/* <div className="container"> */}
+        <Nav.Link as={NavLink} to="/Home">
+          <Slide left>
+            <h1> Liber Populus</h1>
+          </Slide>
+        </Nav.Link>
         <Container maxWidth="lg">
-          <Navbar />
+          <Fade top>
+            <NavbarBootStrap variant="dark">
+              {" "}
+              <Nav className="mx-auto">
+                {routes.map((route) => (
+                  <Nav.Link
+                    key={route.path}
+                    as={NavLink}
+                    to={route.path}
+                    activeClassName="active"
+                    exact
+                  >
+                    <h5>
+                      <i>{route.name}</i>
+                    </h5>
+                  </Nav.Link>
+                ))}
+              </Nav>
+            </NavbarBootStrap>
+          </Fade>
           <br />
           <Route exact path="/">
             {<Redirect to="/Home" />}
           </Route>
-          <Route path="/Home" component={Home}></Route>
-          <Route path="/ScanBallot" component={ScanBallot}></Route>
-          <Route path="/VoterSuppression" component={VoterSuppression}></Route>
-          <Route path="/Volunteer" component={Volunteer}></Route>
-          <Route path="/RegisterToVote" component={RegisterToVote}></Route>
-          <Route path="/TrackBallot" component={TrackBallot}></Route>
-          <Route path="/News" component={News}></Route>
-          <Route
-            path="/AddressVerification"
-            component={AddressVerification}
-          ></Route>
-          <Route path="/LocationsToVote" component={LocationsToVote}></Route>
-          {/* </div> */}
+          {/* Other Routes */}
+          <Zoom top>
+            <Container className="container">
+              {routes.map(({ path, Component }) => (
+                <Route key={path} exact path={path}>
+                  {({ match }) => (
+                    <CSSTransition
+                      in={match != null}
+                      timeout={300}
+                      classNames="page"
+                      unmountOnExit
+                    >
+                      <div className="page">
+                        <Component />
+                      </div>
+                    </CSSTransition>
+                  )}
+                </Route>
+              ))}
+            </Container>
+          </Zoom>
         </Container>
       </Router>
     </div>
