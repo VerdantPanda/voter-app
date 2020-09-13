15a039976372c1ffd3393228f628ed3447273f28
nshussain3 <nshussain3@gmail.com>
2020-09-13 04:11:44 -0400

    ballotErrors complete

--- frontend/voter-app/src/components/VoterSuppression.component.js
+++ frontend/voter-app/src/components/VoterSuppression.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import Iframe from 'react-iframe';
 
 var today = new Date();
 var voteDay = new Date(today.getFullYear(), 10, 3);
@@ -15,6 +16,16 @@ export default class VoterSuppression extends Component {
           Days left.
         </h1>
         <p>You are on the Voter Suppression component!</p>
+        <p2>HTML IFRAME TEST</p2>
+        <Iframe url="https://data.census.gov/cedsci/profile?g=0400000US42"
+        width="100%"
+        height="450px"
+        id="food"
+        className="yes"
+        display="initial"
+        position="relative"
+        sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts"/>
+        
       </div>
     );
   }
